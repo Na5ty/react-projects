953f8b4c53d5237c8d7ed8848a9899e11fbba5f5
Na5ty <thomas.zeides@web.de>
2024-03-01 11:08:35 +0100

    custom react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,10 +1,11 @@
 import Youtube from "./Youtube";
 
 function App() {
+  const username = "thomas";
   return (
     <>
-      <h1>classic react vite app</h1>
-      <h1>classic react vite app</h1>
+      <h1>classic react vite app {2 + 2}</h1>
+      <h1>classic react vite app {username}</h1>
       <Youtube />
     </>
   );
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,4 +1,33 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import React from "react";
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  );
+}
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit Google"
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
