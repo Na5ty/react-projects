cfe6a60ae0678a4d14933b81a235b3f250fd3f60
Na5ty <thomas.zeides@web.de>
2024-03-01 15:20:20 +0100

    color changer

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+            onClick={() => setColor("green")}
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+            onClick={() => setColor("blue")}
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+            onClick={() => setColor("orange")}
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+            onClick={() => setColor("hotpink")}
+            style={{ backgroundColor: "hotpink" }}
+          >
+            Hotpink
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
